dc29909b7372b7ce8732fcc79d591c51c2746f0b
bartodziej777 <bartek3993@gmail.com>
2022-12-18 23:42:26 +0100

    changes in controller

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,21 +1,30 @@
-import mainView from "./mainView";
+import * as model from "./model";
+import navigationView from "./navigationView";
 import newTaskView from "./newTaskView";
 
+const controllNavigation = function (element) {
+  model.state.currentView = element;
+};
+
 const init = function () {
-  mainView.render();
+  //renderowanie widoku startowego
+  navigationView.render();
   newTaskView.render();
 
-  const btnAdd = document.querySelector(".navigation__icon--add");
-  btnAdd.addEventListener("click", function (e) {
-    const newTaskContainer = document.querySelector(".newTask__container");
-    if (!newTaskContainer.classList.contains("newTask__container--active")) {
-      newTaskContainer.classList.add("newTask__container--active");
-      newTaskContainer.classList.remove("newTask__container--unactive");
-    } else {
-      newTaskContainer.classList.remove("newTask__container--active");
-      newTaskContainer.classList.add("newTask__container--unactive");
-    }
-  });
+  //podpiananie event handlerow
+  navigationView.addHandler(controllNavigation);
+
+  // const btnAdd = document.querySelector(".navigation__icon--add");
+  // btnAdd.addEventListener("click", function (e) {
+  //   const newTaskContainer = document.querySelector(".newTask__container");
+  //   if (!newTaskContainer.classList.contains("newTask__container--active")) {
+  //     newTaskContainer.classList.add("newTask__container--active");
+  //     newTaskContainer.classList.remove("newTask__container--unactive");
+  //   } else {
+  //     newTaskContainer.classList.remove("newTask__container--active");
+  //     newTaskContainer.classList.add("newTask__container--unactive");
+  //   }
+  // });
 };
 
 init();
---
+++ src/js/navigationView.js
@@ -0,0 +1,47 @@
+import View from "./view";
+
+class navigationView extends View {
+  _parentElement;
+
+  _generateMarkup(data) {
+    this._parentElement = document.querySelector(".navigation");
+    return `
+      <ul class="navigation__list">
+        <li class="navigation__element" data-element="list">
+          <i class="navigation__icon fa-solid fa-list-ul"></i>
+        </li>
+        <li class="navigation__element" data-element="pinned">
+          <i class="navigation__icon fa-solid fa-thumbtack"></i>
+        </li>
+        <li class="navigation__element" data-element="new">
+          <i
+            class="navigation__icon navigation__icon--add fa-solid fa-plus"
+          ></i>
+        </li>
+        <li class="navigation__element" data-element="search">
+          <i class="navigation__icon fa-solid fa-magnifying-glass"></i>
+        </li>
+        <li class="navigation__element" data-element="trash">
+          <i class="navigation__icon fa-solid fa-trash-can"></i>
+        </li>
+      </ul>`;
+  }
+
+  addHandler(handler) {
+    this._parentElement.addEventListener("click", function (e) {
+      const element = e.target.closest(".navigation__element")?.dataset.element;
+      if (!element);
+      const newTaskContainer = document.querySelector(".newTask__container");
+      if (!newTaskContainer.classList.contains("newTask__container--active")) {
+        newTaskContainer.classList.add("newTask__container--active");
+        newTaskContainer.classList.remove("newTask__container--unactive");
+      } else {
+        newTaskContainer.classList.remove("newTask__container--active");
+        newTaskContainer.classList.add("newTask__container--unactive");
+      }
+      handler(element);
+    });
+  }
+}
+
+export default new navigationView();
